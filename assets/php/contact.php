7f77ac12d5eb3a67f242315441cf5e7e127b588e
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-06-28 14:14:38 -0400

    adding portfolio code

---
+++ assets/php/contact.php
@@ -0,0 +1,26 @@
+<$php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = 'markavella1@gmail.com';
+
+    $email_subject = "New Form Submission";
+
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Message: $message.\n";
+
+    
+    $to = "markavella1@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers = "Reply-to: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
+    
+
+    ?>
